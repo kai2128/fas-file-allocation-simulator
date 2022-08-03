f88200967f6a02585ba6c1bc267cf4b85360e24f
kai <kaienyeo225@gmail.com>
2022-08-03 22:29:53 +0800

    fix: write file error

--- src/composables/animations.ts
+++ src/composables/animations.ts
@@ -112,7 +112,7 @@ export function initAnimation() {
     case 'write':
       fs.value.checkExist(actions.value.file.name)
       // size check
-      if (actions.file.size > fs.value.searchFileInDirectory(actions.value.file.name)!.size) { // only check disk space if new size is larger than old size{
+      if (actions.value.file.size > fs.value.searchFileInDirectory(actions.value.file.name)!.size) { // only check disk space if new size is larger than old size{
         fs.value.checkSpace(actions.value.file.size - fs.value.searchFileInDirectory(actions.value.file.name)!.size)
       }
       setStepsDesc(fatActions.write.steps)
