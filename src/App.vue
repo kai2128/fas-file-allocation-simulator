5beb3add52416122c0198cf9b4164215d420146b
kai <kaienyeo225@gmail.com>
2022-08-11 03:12:55 +0800

    fix: icon path

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ useHead({
     {
       rel: 'icon',
       type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+      href: computed(() => preferredDark.value ? '/fas-file-allocation-simulator/favicon-dark.svg' : '/fas-file-allocation-simulator/favicon.svg'),
     },
   ],
 },
