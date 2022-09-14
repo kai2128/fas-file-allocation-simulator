e29eb2c4b24a7c88bdac172e1ca69c3f837b0227
kai <kaienyeo225@gmail.com>
2022-09-14 12:09:59 +0800

    chore: clean up unuse dep

--- cypress.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  viewportHeight: 1080,
-  viewportWidth: 1920,
-  baseUrl: 'http://localhost:3333',
-  chromeWebSecurity: false,
-})
--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,6 @@ export default defineConfig({
       imports: [
         'vue',
         'vue-router',
-        'vue-i18n',
         'vue/macros',
         '@vueuse/head',
         '@vueuse/core',
